86a8132aac55f791f518b057fece74a2e92c06a1
Dmitry Savelyev <ds2012.65@gmail.com>
2018-05-09 14:50:23 +0300

    remote: fixes

--- remote/jobmanager/slurm/lib/do_submit.bash
+++ remote/jobmanager/slurm/lib/do_submit.bash
@@ -1,5 +1,5 @@
 do_submit() {
-    pre_submit_hook "$@"
+    pre_submit_hook "$@" || exit
 
     SBATCH_OUT=$(sbatch --output="$OUTDIR/%j.out" --error="$OUTDIR/%j.err" -D "$CWD" "${SBATCH_ARGS[@]}" "${BATCH_SCRIPT_ARGS[@]}") || exit
     echo "$SBATCH_OUT" | awk '{ print $4 }'
