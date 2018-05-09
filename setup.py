1dd55ae1de8f91aac3b45dc5c90763e5cf6b0215
Dmitry Savelyev <ds2012.65@gmail.com>
2018-05-09 17:10:11 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
+from functools import partial
+import os
 from setuptools import setup, find_packages
-import pip
-import subprocess
 
 name = 'scaling'  # FIXME: what a terrible name
 
+
 if __name__ == '__main__':
-    subprocess.check_output(['pip', 'install', 'ply'])
-
-    from scaling import exprs as se
-    se._generate_tables()
-
     setup(
         name=name,
         version="0.1",
-        py_modules=['scaling'],
+        packages=[name],
         install_requires=['attrs', 'ply', 'paramiko', 'toolz', 'sympy', 'toposort', 'regex', 'pytoml'],
+        entry_points = {'console_scripts': ['scaling = scaling.__main__:main']}
     )
