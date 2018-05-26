adfedbaed95a737068a45cec3ceb9a1952fc401f
Dmitry Savelyev <ds2012.65@gmail.com>
2018-05-26 13:56:51 +0300

    loadleveler: create scaling-status

--- remote/jobmanager/loadleveler/lib/do_submit.bash
+++ remote/jobmanager/loadleveler/lib/do_submit.bash
@@ -2,5 +2,6 @@ STATUSDIR="$HOME/.scaling-status"
 
 do_submit() {
     JCF="$("$BINDIR/make_jcf" "$CWD" "$OUTDIR" "$@")"
+    mkdir -p "$STATUSDIR"
     "$BINDIR"/llsubmit-with-monitor "$JCF" "$BINDIR/monitor" "$STATUSDIR"
 }
